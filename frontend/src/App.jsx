b4ba64747b27dcb8269dd254924527e58b73bd65
ggabi40 <gabibc2000@hotmail.com>
2025-04-15 17:13:23 -0300

    Inicializando el proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1>Hola!</h1>
+      <p>TPI Programacion 3</p>
+    </>
+  )
+}
+
+export default App
