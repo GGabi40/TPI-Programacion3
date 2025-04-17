4eb05ced197afc2153649f70d3dd926d0c7d2615
ggabi40 <gabibc2000@hotmail.com>
2025-04-17 18:21:08 -0300

    Style: add standar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,11 @@ function App() {
     <>
       <NavBar />
       <h1>Hola!</h1>
+      <h2>Aloo!</h2>
       <p>TPI Programacion 3</p>
       <h1>holaaa</h1>
+      <button>Enviar</button>
+      <button className="action">Crear cuenta</button>
     </>
   )
 }
