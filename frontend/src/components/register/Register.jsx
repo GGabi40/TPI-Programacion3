368e37e6568988103d2a8fe65c8a33e71f138604
ggabi40 <gabibc2000@hotmail.com>
2025-05-06 17:20:11 -0300

    form style modified

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import TopNav from '../nav/TopNav'
-import Footer from '../footer/Footer'
+import React from "react";
+import Footer from "../footer/Footer";
+
+import logo from "../../assets/img/logo/Logo-InkLink.webp";
+import { Link } from "react-router";
 
 const Register = () => {
-    return (
-        <div>
-            <TopNav />
+  return (
+    <div>
+      <div className="background-animated">
+        <div className="light-orb"></div>
+      </div>
 
-            <div className='form-container'>
-            <h2 id='crearCuenta'>CREE SU CUENTA</h2>
-            <br />
-                <form>
-                    <label>Nombre de Usuario:</label>
-                    <input type="text" />
-                    
-                    <label>Ingrese su email:</label>
-                    <input type="email" />
-                    
-                    <label>Ingrese su clave:</label>
-                    <input type="password" />
-                   
-                    <label>Confirmar clave:</label>
-                    <input type="password" />
-                    
-                    <label>Fecha de nacimiento</label>
-                    <input type="date" />
-                    
-                    <p>¿Ya tiene una cuenta? <a href="" id='redireccion'>Iniciar sesion</a></p>
-                    <button type="submit">Crear Cuenta</button>
-                </form>
-                </div>
-                
-                <Footer />
+      <div className="form-container margin">
+        <div className="logo-form">
+          <img src={logo} alt="Logo Inklink" />
         </div>
-    )
-}
 
-export default Register
+        <h2 id="crearCuenta" className="text-align">
+          CREE SU CUENTA
+        </h2>
+        <br />
+        <form>
+          <label>Nombre de Usuario:</label>
+          <input type="text" placeholder="Ej: juan_perez" />
+
+          <label>Ingrese su email:</label>
+          <input type="email" placeholder="email@ejemplo.com" />
+
+          <label>Ingrese su clave:</label>
+          <input type="password" placeholder="Contraseña segura" />
+
+          <label>Confirmar clave:</label>
+          <input type="password" placeholder="Confirmar contraseña" />
+
+          <label>Fecha de nacimiento</label>
+          <input type="date" />
+
+          <button type="submit">Crear Cuenta</button>
+
+          <p>
+            ¿Ya tiene una cuenta?{" "}
+            <Link to="/" id="redireccion">
+              Iniciar sesión
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Register;
