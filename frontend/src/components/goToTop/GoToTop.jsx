0e5980b02473badd7cb925b5fa319a8576f5570e
ggabi40 <gabibc2000@hotmail.com>
2025-05-06 16:40:59 -0300

    goToTop component changed

--- frontend/src/components/goToTop/GoToTop.jsx
+++ frontend/src/components/goToTop/GoToTop.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const GoToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-    const handleScrollTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-          })
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 20) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
+  };
+
+  // cada vez que se hace "scroll", ejecuta la func toggleVisibility
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const handleScrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <button id='goTop' onClick={handleScrollTop}>
-        <FontAwesomeIcon icon={faArrowUp} />
+    <button 
+      id="goTop" 
+      onClick={handleScrollTop}
+      style={
+        {
+          visibility: isVisible ? 'visible' : 'hidden',
+          opacity: isVisible ? 1 : 0,
+          transition: "opacity 0.4s ease, visibility 0.4s ease"
+        }
+        }
+    >
+      <FontAwesomeIcon icon={faArrowUp} />
     </button>
-  )
-}
+  );
+};
 
-export default GoToTop
+export default GoToTop;
