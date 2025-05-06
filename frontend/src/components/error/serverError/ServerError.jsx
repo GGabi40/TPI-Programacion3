051de5273984be0f4b0fb425f374b09aa18d3ea5
ggabi40 <gabibc2000@hotmail.com>
2025-05-06 18:07:01 -0300

    serverError component added

--- frontend/src/components/error/serverError/ServerError.jsx
+++ frontend/src/components/error/serverError/ServerError.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ErrorLayout from "../layout/ErrorLayout";
+
+import ServerErrorImg from "../../../assets/img/error-img/error-500.webp";
 
 const ServerError = () => {
   return (
-    <div>ServerError</div>
+    <ErrorLayout
+      code="500"
+      title="¡Uy! Algo salió mal"
+      description="Ocurrió un problema inesperado en nuestro servidor. Estamos trabajando para solucionarlo. Por favor, intentá de nuevo más tarde."
+      image={ServerErrorImg}
+    />
   )
 }
 
