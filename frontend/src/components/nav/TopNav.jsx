48e466c4a755e34e85813ccce09859375ff130a6
elenarrios <elenarios2005@gmail.com>
2025-05-10 18:55:08 -0300

    fixed TopNav - Register button

--- frontend/src/components/nav/TopNav.jsx
+++ frontend/src/components/nav/TopNav.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../../styles/nav/topNav.css'
-import { Link } from 'react-router'
+import React from 'react';
+import '../../styles/nav/topNav.css';
+import { Link } from 'react-router';
+import { useState } from 'react';
 
 import logo from '../../assets/img/logo/Logo-InkLink.webp';
+import Register from '../register/Register';
 
-const TopNav = () => {
+const TopNav = ({ showRegisterButton }) => {
   return (
     <nav id='topNav'>
         <div className="logo top">
@@ -14,10 +16,12 @@ const TopNav = () => {
 
         <div className="buttons top">
             <Link to="/" className="link-button">Login</Link>
+            {showRegisterButton && (
             <Link to="/register" className="link-button">Register</Link>
+            )}
         </div>
     </nav>
-  )
-}
+  );
+};
 
-export default TopNav
+export default TopNav;
