399d9f796f5686ea93465004c73f1226c58e58a7
elenarrios <elenarios2005@gmail.com>
2025-05-15 17:04:18 -0300

    review-validations

---
+++ backend/src/routes/review.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createReview, deleteReview, getAllReviews, getReviewById, updateReview } from "../services/review.services.js";
+
+const router = Router();
+router.post("/reviews", createReview);
+router.get("/reviews", getAllReviews);
+router.get("/reviews/:id", getReviewById);
+router.put("/reviews/:id", updateReview);
+router.delete("/reviews/:id", deleteReview);
+
+export default router;
