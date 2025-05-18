cc87254e203436b8dffb1a6b060e58f58b86aded
ggabi40 <gabibc2000@hotmail.com>
2025-05-18 15:43:39 -0300

    Add a search bar style

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-
-
 const Search = ({ onSearch }) => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
 
-    const handleSearch = () => {
-        onSearch(input); 
-    };
+  const handleSearch = () => {
+    onSearch(input);
+  };
 
   return (
-    <div className='searchbar'>
-      <input type="text" id="search" placeholder="Buscar club..." value={input} onChange={(e) => setInput(e.target.value)}/>
-      <button onClick={handleSearch} className='searchbtn'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
-      
+    <div className="search-bar-container">
+      <div className="search-bar">
+        <input
+          type="text"
+          id="search"
+          placeholder="Buscar club..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleSearch} className="search-btn">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
