ab34e905fd36df06d8da465f2fd0467b171a9e4c
celeste calvo <cele_calvo_98@outlook.es>
2025-05-25 11:00:09 -0300

    remove modal component, update newclub and modifyclub components and create clubs component

--- frontend/src/components/newclub/NewClub.jsx
+++ frontend/src/components/newclub/NewClub.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import ClubForm from "../clubForm/ClubForm";
-
+import { useFetch } from "../hook/UseFetch";
 import { useNavigate } from "react-router";
 
-const NewClub = () => {
+const { post } = useFetch("/clubs");
 
+const NewClub = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (data) => {
-    const res = await fetch("http://localhost:3000/clubs", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    if (res.ok) {
-      const created = await res.json();
-      navigate(`/clubDetails/${created.id}`);
-    };
+    const created = await post(data);
+    navigate(`/clubDetails/${created.id}`);
   }
-  return (
-    <ClubForm
-      mode="create"
-      onSubmit={handleSubmit}
-    />)
+  return <ClubForm mode="create" onSubmit={handleSubmit} />
 }
 
 export default NewClub
-
-
-
-
